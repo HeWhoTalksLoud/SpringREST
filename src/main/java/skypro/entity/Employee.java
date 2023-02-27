d2471bff62930bb57920e75b29c9d753f02abac8
Pasha <nikolenko.pavel@gmail.com>
2023-02-28 02:52:57 +0300

    Начальный вариант.

---
+++ src/main/java/skypro/entity/Employee.java
@@ -0,0 +1,60 @@
+package skypro.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "person")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String first_name;
+    @Column(name = "department")
+    private int department;
+    @Column(name = "salary")
+    private float salary;
+
+    public Employee() {}
+
+    public Employee(int id, String first_name, int department, float salary) {
+        this.id = id;
+        this.first_name = first_name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+}
